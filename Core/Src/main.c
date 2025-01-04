b9203b51343a11e908b28a9c990040c78cc855e4
Furqan Riansyah <putra.furqan12@gmail.com>
2025-01-04 21:23:07 +0700

    update: order change

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -38,7 +38,7 @@
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
 #define FILTER_LENGTH 50
-#define FIR_LENGTH      11
+#define FIR_LENGTH      8
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -51,7 +51,7 @@
 /* USER CODE BEGIN PV */
 
 // FIR filter instances ========================
-float32_t fir_coefficients[FIR_LENGTH] = {-0.0220, -0.0339, 0.0072, 0.1235, 0.2618, 0.3244, 0.2618, 0.1235, 0.0072, -0.0339, -0.0220};
+float32_t fir_coefficients[FIR_LENGTH] = {0.0714,0.1030,0.1455,0.1711,0.1711,0.1455,0.1030,0.0714};
 arm_fir_instance_f32 fir_instance_accel_x;
 arm_fir_instance_f32 fir_instance_accel_y;
 arm_fir_instance_f32 fir_instance_accel_z;
